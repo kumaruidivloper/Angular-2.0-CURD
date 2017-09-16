a3f247b9da4a2d4b184d1ee2dc85d234fba764af
Kumaravel Shanmugam <kumaravel.shanmugam@volvo.com>
2017-09-16 17:00:58 +0530

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular 2 CURD Operation with an Array';
+  employees = [
+    {name:"Kumaravel", position:"Developer"},
+    {name:"Vasuki", position:"Manager"},
+    {name:"Swathy", position:"Designer"}
+  ];
+
+  model:any={};
+  model2:any={};
+  msg:any="";
+
+  addEmployee(){
+    this.employees.push(this.model);
+    this.model = {};
+    this.msg = "Record is sucessfully added.....";
+    setTimeout(function(){
+
+      }, 3000);
+  }
+  deleteEmployee(i){
+    this.employees.splice(i,1);
+    this.msg = "Record is sucessfully deleted.....";
+  }
+  myValue;
+  editEmployee(k){
+    this.model2.name = this.employees[k].name;
+    this.model2.position = this.employees[k].position;
+    this.myValue = k;
+  }
+  updateEmployee(){
+     let k = this.myValue;
+     for(let i=0; i<this.employees.length; i++){
+       if(i==k){
+         this.employees[i] = this.model2;
+         this.model2 = {};
+         this.msg = "Record is sucessfully updated.....";
+       }
+     }
+  }
+
+  clickMe(){
+    this.msg = "";
+}
+
+}
